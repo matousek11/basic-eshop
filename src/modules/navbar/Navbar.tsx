0183c76e47df876a95d99efdbac12e4ac917f7b4
Lukas Matousek <matousek1199@gmail.com>
2023-07-29 11:02:15 +0200

    Basic navbar

--- src/modules/navbar/Navbar.tsx
+++ src/modules/navbar/Navbar.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <nav className="flex justify-center items-center gap-x-5 p-4">
+      <p>Eshop</p>
+      <p>Offers</p>
+      <img className="h-6" src="/icons/cart-shopping-solid.svg" />
+    </nav>
+  )
 }
 
 export default Navbar
